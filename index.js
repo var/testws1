397a671f64180da088bed718d6e378a8d455fc6a
Vimal Atreya Ramaka <vimal@ramaka.com>
2020-10-22 15:38:23 -0400

    ..

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const morgan = require('morgan')
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(morgan('combined'))
+  .get('/', (req, res) => {
+    console.log("get", JSON.stringify(req));
+    res.send('hi');
+  }).post('/', ((req, res) => {
+    console.log("post", JSON.stringify(req));
+    res.send('hi');
+  })).put('/', ((req, res) => {
+    console.log("post", JSON.stringify(req));
+    res.send('hi');
+  }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
