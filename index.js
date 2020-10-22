f3586f613652a6ff19be77ce6de93b830eb7f772
Vimal Atreya Ramaka <vimal@ramaka.com>
2020-10-22 15:50:48 -0400

    bp

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 express()
   .use(morgan('combined'))
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json())
   .get('/', (req, res) => {
-    console.log("get", JSON.stringify(req));
+    console.log("===============GET=====================")
+    console.log("body ======> ", JSON.stringify(req.body));
+    console.log("headers ======> ", JSON.stringify(req.headers));
     res.send('hi');
   }).post('/', ((req, res) => {
-    console.log("post", JSON.stringify(req));
+    console.log("===============POST=====================")
+    console.log("body ======> ", JSON.stringify(req.body));
+    console.log("headers ======> ", JSON.stringify(req.headers));
     res.send('hi');
   })).put('/', ((req, res) => {
-    console.log("put", JSON.stringify(req));
+    console.log("===============PUT=====================")
+    console.log("body ======> ", JSON.stringify(req.body));
+    console.log("headers ======> ", JSON.stringify(req.headers));
     res.send('hi');
   }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
